d390b2565ccb30c7e5d6fc8b300fbe1d26191359
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-03-30 15:58:01 +0100

    Mod Seleccionar Liga a Fetchear

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Header } from "../common/Header"
 import LigaEsp from "../../public/img/LaLigaTransparente.png"
 import LigaIng from "../../public/img/PremierLeague.png"
@@ -8,12 +8,52 @@ import LigaAl from "../../public/img/BundesligaLogo.png"
 
 export const Main = () => {
     //UseState -> SetLiga()
-    const [liga, setLiga] = useState([])
-    //Array de ligas.
-    const ligas = [LigaEsp, LigaIng, LigaItl, LigaFr, LigaAl]
-    useEffect(() => {
+    const [liga, setLiga] = useState(0)
+    const [inputVal, setInputVal] = useState("")
 
-    }, [liga])
+    //Array de ligas.
+    const ligas = [{ nombre: "La Liga (ESP)", img: LigaEsp, id: "" }, { nombre: "Premiere League (ENG)", img: LigaIng, id: "" }, { nombre: "Lega Calcio(ITL)", img: LigaItl, id: "" },
+    { nombre: "Ligue 1 (FRA)", img: LigaFr, id: "" }, { nombre: "Bundesliga (ALE)", img: LigaAl, id: "" }]
+
+    const mostrarLiga = (index) => {
+        return (
+            <div className="row leagues">
+                <div className="col-md-2 change-league">
+                    <button type="button" class="btn btn-secondary btn-lg" onClick={() => {
+                        if (liga > 0) {
+                            setLiga(liga - 1);
+                            setInputVal(ligas[liga].nombre)
+                        } else {
+                            setLiga(ligas.length - 1);
+                            setInputVal(ligas[liga].nombre)
+
+                        }
+                    }}>LIGA ANTERIOR</button >
+
+                </div>
+
+                <div className="col-md-8 league-icon">
+                    <img src={ligas[index].img} />
+                </div>
+                <div className="col-md-2 change-league">
+                    <button type="button" class="btn btn-secondary btn-lg" onClick={() => {
+                        if (liga < ligas.length - 1) {
+                            setLiga(liga + 1);
+                            setInputVal(ligas[liga].nombre)
+
+                        } else {
+                            setLiga(0);
+                            setInputVal(ligas[liga].nombre)
+
+                        }
+                    }}>LIGA SIGUIENTE</button >
+
+                </div>
+            </div>
+        )
+
+    }
+
 
     return (
         <>
@@ -21,25 +61,21 @@ export const Main = () => {
 
             <main>
                 <div className="container">
-                    <div className="row league-icon">
-                        <div className="col-md-12">
-                            <img />
-                        </div>
+                    {mostrarLiga(liga)}
+                </div>
+                <div className="row">
+                    <div className="col-md-2"></div>
+                    <div className="col-md-8 league-submit">
+                        <button type="button" class="btn btn-primary btn-lg">Ir a la {ligas[liga].nombre}</button>
+
                     </div>
-                    <div className="row league-input">
-                        <div className="col-md-8">
-                            <input></input>
-                        </div>
-                    </div>
-                    <div className="row league-button">
-                        <div className="col-md-4">
-                            <button></button >
-                        </div>
-                    </div>
+                    <div className="col-md-2"></div>
 
                 </div>
 
-            </main>
+
+
+            </main >
 
             <footer>
 
