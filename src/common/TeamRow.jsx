7be143ebcc883c2256c381206a8e5b31d69d1edc
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-04-03 19:53:34 +0200

    New Components

---
+++ src/common/TeamRow.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const TeamRow = (props) => {
+    return (
+        <div>
+            <div id='team-logo'>
+                <img src={ }>
+            </div>
+            <div id='team-name'></div>
+            <div id='team-score'></div>
+            <div id='team-trophies'></div>
+
+        </div>
+    )
+}
