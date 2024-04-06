9cf15ceff87fa29df5b504b1f4352447b9a5e1ec
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-04-06 18:04:52 +0200

    Arreglando commit erroneo

--- src/common/TeamRows.jsx
+++ src/common/TeamRows.jsx
@@ -4,11 +4,11 @@ import { TeamRow } from './TeamRow'
 export const TeamRows = (props) => {
     const leagueData = props.data
     /*Aquí hacemos la peticion de todos los equipos de la liga, en las props nos viene la url de la liga. */
-
+    console.log(leagueData)
 
     return (
         <div className='col-md-6 team-rows-container'>
-            <TeamRow />
+            <TeamRow data={leagueData} />
 
         </div>
     )
