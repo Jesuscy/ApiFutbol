dcd99c24c7b9ba589b042d79cefb59709f4f54fc
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-04-08 17:54:04 +0200

    Mod Add Peticiones al ServerIntermedio

--- servidor-intermedio/server.cjs
+++ servidor-intermedio/server.cjs
@@ -4,6 +4,8 @@ const axios = require('axios');
 const app = express();
 const PORT = 3002;
 
+//IMPORTANTE PREGUNTAR
+app.use(express.json());
 
 //CONFIGURO EL HEADER CON EL QUE HACER LAS PETICIONES A LA API.
 app.use((req, res, next) => {
@@ -16,11 +18,12 @@ app.use((req, res, next) => {
 //POST --> RECIBE DE REACT URL Y DEVUELVE EL RESULTADO DE LA PETICIÓN
 app.post('/api/football-info', async (req, res) => {
     //OBTEGO LA URL QUE MANDO EN LA REQUEST DESDE REACT.
+
     try {
-        const url = req.body
+        const { url } = req.body
         const response = await axios.get(url, {
             headers: {
-                'X-Auth-Token': '13f4273034d64c8d93015d3881f2457a' // Reemplaza 'tu_token_de_autorizacion' con tu token real
+                'X-Auth-Token': '13f4273034d64c8d93015d3881f2457a'
             }
         });
         res.json(response.data);
