422c72928a71cdd5a8cd3357328035f7a23c0575
Jesus Carrasco <Jesuscarrascoyel.mur@gmail.com>
2024-04-08 13:30:18 +0200

    Añadiendo Server Intermedio controlador de peticiones

--- src/common/TeamRows.jsx
+++ src/common/TeamRows.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { TeamRow } from './TeamRow'
-
+import axios from 'axios'
 export const TeamRows = (props) => {
     const leagueData = props.data
     /*Aquí hacemos la peticion de todos los equipos de la liga, en las props nos viene la url de la liga. */
-
+    console.log(leagueData.url)
+    const config = {
+        headers: {
+            'X-Auth-Token': '13f4273034d64c8d93015d3881f2457a'
+        }
+    }
+    const response = axios.get('http://api.football-data.org/v4/competitions/PD/teams', config)
+    console.log(response)
 
     return (
         <div className='col-md-6 team-rows-container'>
-            <TeamRow />
+            <TeamRow data={leagueData} />
 
         </div>
     )
